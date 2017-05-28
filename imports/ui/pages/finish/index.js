21cbeb4188b5249281f3b5ff2f50cf7533ff789c
Ugur Oruc <uoruc@bordobt.com>
2017-05-28 11:43:02 +0300

    Play again buttons added

--- imports/ui/pages/finish/index.js
+++ imports/ui/pages/finish/index.js
@@ -4,3 +4,10 @@ import './index.html';
 Template.App_finish.onRendered(()=>{
 	$('ul.tabs').tabs();
 })
+
+Template.App_finish.events({
+	'click a.playAgain':function(event){
+		event.preventDefault();
+		document.location.reload(true);
+	}
+})
