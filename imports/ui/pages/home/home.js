a1302fefabc632cac03b80220f37e6ed2d8338e2
Ugur Oruc <uoruc@bordobt.com>
2017-05-28 11:33:56 +0300

    Timer bug fixed and timer starts after images loaded

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,38 +1,54 @@
 import './home.html';
+// Loading Component
 import '../../components/loading';
-import '../finish/'
+// Finish Page
+import '../finish/';
+// Database Import
 import { Community } from '/imports/api/community/community.js';
 
 var Shuffle = require('shuffle-array');
 
 Template.App_home.onCreated(function(){
-	// counter starts at 0
+	// ReactiveVar Definitions
 	_self = this;
+
 	_self.answers = new ReactiveVar();
 	_self.question = new ReactiveVar();
 	_self.wrong_answer = new ReactiveVar();
 	_self.true_answers = new ReactiveVar(new Array());
 	_self.timer = new ReactiveVar();
+	_self.timerId = new ReactiveVar();
 
+	// First question get
 	Meteor.call('community.question.get',function(err,resp){
 		_self.question.set(resp.question);
 		_self.answers.set(Shuffle(resp.answers));
+
 	});
 
 	Meteor.subscribe('community');
 });
 
 Template.App_home.onRendered(function(){
+	// Timer Init for first time opening page
 	_self = this;
 	_self.timer.set(15);
-	setInterval(function(){
+	var timerId = Meteor.setInterval(function(){
 		let time = _self.timer.get();
 		if(time <= 0 ){
 			_self.wrong_answer.set(_self.question.get());
 			_self.true_answers.set(false);
 		}
-		_self.timer.set((time - 0.10).toFixed(1));
-	},100)
+		_self.timer.set((time - 1));
+	},1000)
+	_self.timerId.set(timerId);
+
+
+});
+
+Template.App_home.onDestroyed(function(){
+	_self = this;
+	Meteor.clearInterval(_self.timerId.get())
 })
 
 Template.App_home.helpers({
@@ -58,28 +74,62 @@ Template.App_home.helpers({
 
 Template.App_home.events({
 	'click a.answer'(event, instance) {
+		// Question And Answer Get
 		const question = instance.question.get();
 		const answer = question._id == this._id;
+
+		// Clear The Timer
+		var timerId = _self.timerId.get();
+		Meteor.clearInterval(timerId);
+
+		// Check if answer is true
 		if(!answer){
 			instance.wrong_answer.set(question);
 			instance.true_answers.set(false);
 			return false;
 		};
+
+		// Get Guessed Answers
 		let already_guessed = instance.true_answers.get();
+		// Push new answer to array
 		already_guessed.push(this._id);
 		instance.true_answers.set(already_guessed);
+
+		// If all questions is finished
 		if(Community.find().count() == already_guessed.length){
 			_self.true_answers.set(false);
 			FlowRouter.go('/win');
 			return false;
 		}
+		// Clear old question and answers
 		instance.question.set(false);
 		instance.answers.set(false);
+
+		// Get New Question and answers
 		Meteor.call('community.question.get',already_guessed,function(err,resp){
 			if(!err){
-				_self.timer.set(15);
+				// New Question and Answers added
 				_self.question.set(resp.question);
 				_self.answers.set(Shuffle(resp.answers));
+
+				// Check image status
+				$('<img />').attr({'src':resp.question.image})
+					.on('load', function() {
+						// Timer Has Been Set
+						_self.timer.set(15);
+						// New Timer init
+						timerId = Meteor.setInterval(function(){
+							let time = _self.timer.get();
+							if(time <= 0 ){
+								_self.wrong_answer.set(_self.question.get());
+								_self.true_answers.set(false);
+							}
+							_self.timer.set((time-1));
+						},1000);
+						_self.timerId.set(timerId);
+					})
+
+
 			}
 		})
 
