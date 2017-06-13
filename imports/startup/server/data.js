5b5acf73338e3072181dd58ac3801d05bf09dd23
Ugur Oruc <uoruc@bordobt.com>
2017-06-13 21:28:31 +0300

    Genders Added for answers
    No offense please just gender 😊

--- imports/startup/server/data.js
+++ imports/startup/server/data.js
@@ -2,171 +2,204 @@ export const CommunityMembers = [
 	{
 		"name":"Emre Efendioğlu",
 		"image":"https://pbs.twimg.com/profile_images/474511791028834305/RH5TYEk1.jpeg",
-		"twitter":"emreefendioglu"
+		"twitter":"emreefendioglu",
+		"gender":"m"
 	},
 	{
 		"name":"Çağıl Uluşahin",
 		"image":"https://pbs.twimg.com/profile_images/2209335839/0gQbMvJv",
-		"twitter":"cagilulusahin"
+		"twitter":"cagilulusahin",
+		"gender":"f"
 	},
 	{
 		"name":"Yiğit Güler",
 		"image":"https://pbs.twimg.com/profile_images/705348432387420160/thw_UAGX.jpg",
-		"twitter":"hyigitguler"
+		"twitter":"hyigitguler",
+		"gender":"m"
 	},
 	{
 		"name":"Cihan Okyay",
 		"image":"https://pbs.twimg.com/profile_images/2553736706/p2pzktfm4vuv0q26ior1.jpeg",
-		"twitter":"cihann"
+		"twitter":"cihann",
+		"gender":"m"
 	},
 	{
 		"name":"Oktay Elipek",
 		"image":"https://pbs.twimg.com/profile_images/695600433839149056/4NbH-5h3.jpg",
-		"twitter":"oktayelipek"
+		"twitter":"oktayelipek",
+		"gender":"m"
 	},
 	{
 		"name":"Kerem Sevencan",
 		"image":"https://pbs.twimg.com/profile_images/826558530136182785/gpzcoEbi.jpg",
-		"twitter":"keremciu"
+		"twitter":"keremciu",
+		"gender":"m"
 	},
 	{
 		"name":"Üstün Özgür",
 		"image":"https://pbs.twimg.com/profile_images/762702823020781568/nGZzb385.jpg",
-		"twitter":"ustunozgur"
+		"twitter":"ustunozgur",
+		"gender":"m"
 	},
 	{
 		"name":"Adem İlter",
 		"image":"https://pbs.twimg.com/profile_images/850289563184623616/HPZ58mBd.jpg",
-		"twitter":"ademilter"
+		"twitter":"ademilter",
+		"gender":"m"
 	},
 	{
 		"name":"Furkan Açıkgöz",
 		"image":"https://pbs.twimg.com/profile_images/856430655814733824/PHncGfs9.jpg",
-		"twitter":"furkanacikgoz"
+		"twitter":"furkanacikgoz",
+		"gender":"m"
 	},
 	{
 		"name":"Mehmet Oğuz Arıkan",
 		"image":"https://pbs.twimg.com/profile_images/378800000154040931/5d19e6b3df3d5aeca4852e3f8ef5372c.jpeg",
-		"twitter":"ouzar"
+		"twitter":"ouzar",
+		"gender":"m"
 	},
 	{
 		"name":"Cem Ekici",
 		"image":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/000/0e0/2d3/0433575.jpg",
-		"linkedin":"https://www.linkedin.com/in/cem-ekici-562b1419/photo/"
+		"linkedin":"https://www.linkedin.com/in/cem-ekici-562b1419/photo/",
+		"gender":"m"
 	},
 	{
 		"name":"Mert Kavi",
 		"image":"https://pbs.twimg.com/profile_images/581177983941144576/S_Mp0ZGV.png",
-		"twitter":"mkavi"
+		"twitter":"mkavi",
+		"gender":"m"
 	},
 	{
 		"name":"Fatih Kadir Akın",
 		"image":"https://pbs.twimg.com/profile_images/763012516561780736/eK6n4vgr.jpg",
-		"twitter":"fkadev"
+		"twitter":"fkadev",
+		"gender":"m"
 	},
 	{
 		"name":"Doğukan Güven Nomak",
 		"image":"https://pbs.twimg.com/profile_images/646407646241648640/optfD5nh.jpg",
-		"twitter":"dnomak"
+		"twitter":"dnomak",
+		"gender":"m"
 	},
 	{
 		"name":"Fatih Acet",
 		"image":"https://pbs.twimg.com/profile_images/839895750481948673/QzmwOIzy.jpg",
-		"twitter":"fatihacet"
+		"twitter":"fatihacet",
+		"gender":"m"
 	},
 	{
 		"name":"Fatih Erikli",
 		"image":"https://avatars0.githubusercontent.com/u/1220743?v=3&s=400",
-		"twitter":"fthrkl"
+		"twitter":"fthrkl",
+		"gender":"m"
 	},
 	{
 		"name":"Hamza Şamlıoğlu",
 		"image":"https://pbs.twimg.com/profile_images/803307698397782016/h2LPCsIp.jpg",
-		"twitter":"TEAkolik"
+		"twitter":"TEAkolik",
+		"gender":"m"
 	},
 	{
 		"name":"Serdar Doğruyol",
 		"image":"https://pbs.twimg.com/profile_images/797044790655381504/9oR_xQl4.jpg",
-		"twitter":"sdogruyol"
+		"twitter":"sdogruyol",
+		"gender":"m"
 	},
 	{
 		"name":"Bilgem Cakir",
 		"image":"https://pbs.twimg.com/profile_images/530566318374662144/HcKYp24a.jpeg",
-		"twitter":"BilgemCakir"
+		"twitter":"BilgemCakir",
+		"gender":"m"
 	},
 	{
 		"name":"Fatih Turan",
 		"image":"http://fatihturan.com/img/covers/homepage.jpg",
-		"twitter":"fatihturan"
+		"twitter":"fatihturan",
+		"gender":"m"
 	},
 	{
 		"name":"Bilal Çınarlı",
 		"image":"https://pbs.twimg.com/profile_images/454945791933562880/ZEjyFHbZ.jpeg",
-		"twitter":"bcinarli"
+		"twitter":"bcinarli",
+		"gender":"m"
 	},
 	{
 		"name":"Jaana Burcu Doğan",
 		"image":"https://pbs.twimg.com/profile_images/866848888967368704/cnl5cb3J.jpg",
-		"twitter":"rakyll"
+		"twitter":"rakyll",
+		"gender":"f"
 	},
 	{
 		"name":"Nesrin Kalender",
 		"image":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/000/0eb/21d/222f88b.jpg",
-		"twitter":"nesrinkalender"
+		"twitter":"nesrinkalender",
+		"gender":"f"
 	},
 	{
 		"name":"Fatih Arslan",
 		"image":"https://pbs.twimg.com/profile_images/617484206512324608/9B5_6E_g.jpg",
-		"twitter":"fatih"
+		"twitter":"fatih",
+		"gender":"m"
 	},
 	{
 		"name":"Burak Yiğit Kaya",
 		"image":"https://pbs.twimg.com/profile_images/569981654765039616/XBRdWMOS.jpeg",
-		"twitter":"madbyk"
+		"twitter":"madbyk",
+		"gender":"m"
 	},
 	{
 		"name":"Hüseyin Mert",
 		"image":"https://pbs.twimg.com/profile_images/818008271059636224/tBNSMTjf.jpg",
-		"twitter":"hmert"
+		"twitter":"hmert",
+		"gender":"m"
 	},
 	{
 		"name":"Hana Kamer",
 		"image":"https://pbs.twimg.com/profile_images/872141565380186113/TeDOZDUx.jpg",
-		"twitter":"terskaplumbaa"
+		"twitter":"terskaplumbaa",
+		"gender":"f"
 	},
 	{
 		"name":"Burak Can",
 		"image":"https://pbs.twimg.com/profile_images/744812523778539520/FpnZkkju.jpg",
-		"twitter":"neoberg"
+		"twitter":"neoberg",
+		"gender":"m"
 	},
 	{
 		"name":"Alpcan Aydın",
 		"image":"https://pbs.twimg.com/profile_images/844197944224026625/a2eKTDfV.jpg",
-		"twitter":"alpcanaydin"
+		"twitter":"alpcanaydin",
+		"gender":"m"
 	},
 	{
  		"name": "Semih Basmacı" ,
  		"image": "https://hipolabs.com/static/img/team/semih.jpg" ,
  		"github": "mortargrind",
- 		"ipucu": "Hipolabs Web Application Developer",
- 		"hakkinda": "Semih is the second half of our effective front-end team, where he specializes in web application infrastructure and bringing responsive sites to life. His background in team leadership makes him an effective communicator, a useful skill when discussing complex web applications. When he is not coding, Semih enjoys MMORPGs."
+		"gender":"m",
+ 		"hint": "Hipolabs Web Application Developer",
+ 		"about": "Semih is the second half of our effective front-end team, where he specializes in web application infrastructure and bringing responsive sites to life. His background in team leadership makes him an effective communicator, a useful skill when discussing complex web applications. When he is not coding, Semih enjoys MMORPGs."
  	},
   {
 		"name":"Tuna Vargı",
 		"image":"https://pbs.twimg.com/profile_images/668788508677554176/v6jsI7pM.jpg",
-		"twitter":"tunavargi"
+		"twitter":"tunavargi",
+		"gender":"m"
 	},
 	{
 		"name":"Yalçın Çakır",
 		"image":"http://odatv.com/images/2017_02/2017_02_03/yalcin-cakir-muge-anliya-rakip-oluyor-0302171200_l2.jpg",
-		"twitter":"yalcincakir"
+		"twitter":"yalcincakir",
+		"gender":"m"
 	},
   {
 		"name": "Λbdullah Uğraşkan" ,
 		"image": "https://pbs.twimg.com/profile_images/855148047063502849/GDuRF9Yj.jpg" ,
 		"twitter": "apoStyLEE",
-		"ipucu": "Trabzon SiberUzay",
-		"hakkinda": "Yazılım Zanaatkarı for SiberUzay 🚀 Trabzon"
+		"gender":"m",
+		"hint": "Trabzon SiberUzay",
+		"about": "Yazılım Zanaatkarı for SiberUzay 🚀 Trabzon"
 	}
 
 
